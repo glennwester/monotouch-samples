efa2f07abb6df13ea545f8451b3cc4e4a96aa7b9
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-08-12 20:05:11 +0400

    added new viewcontrollers

---
+++ UICatalog/UICatalog/ViewControllers/Search/SearchControllers/SearchBarEmbeddedInNavigationBarViewController.cs
@@ -0,0 +1,15 @@
+using System;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace UICatalog
+{
+	public partial class SearchBarEmbeddedInNavigationBarViewController : UITableViewController
+	{
+		public SearchBarEmbeddedInNavigationBarViewController (IntPtr handle)
+			: base (handle)
+		{
+		}
+	}
+}
